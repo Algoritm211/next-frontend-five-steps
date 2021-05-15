afb5b84fb7d90cc0121d29429384b89eb4843628
alexey_horbunov <algoritm211@gmail.com>
2021-05-15 18:56:30 +0300

    Remove localStorage logic from auth-thunks.js

--- store/auth-reducer/auth-thunks.js
+++ store/auth-reducer/auth-thunks.js
@@ -13,7 +13,6 @@ import { deleteCookie } from '../../components/util-funcs/remove-cookie'
 export const registerUser = (userData) => async (dispatch) => {
   try {
     await authAPI.registration(userData)
-
     dispatch(loginUser(userData.email, userData.password))
     dispatch(registrationSuccess())
   } catch (error) {
@@ -26,13 +25,11 @@ export const loginUser = (email, password) => async (dispatch) => {
   try {
     dispatch(setAppReady(false))
     const data = await authAPI.login(email, password)
-    await localStorage.setItem('authToken', data.token)
     dispatch(setUserAuthData(data.user))
     window.location.assign('/')
-    // document.cookie = `authToken=${data.token}; Expires=${new Date(Date.now() + 86400e3)}; Path=/; Domain=localhost;`
     dispatch(setAppReady(true))
   } catch (error) {
-    dispatch(setAuthError(error.response.data.message))
+    dispatch(setAuthError(error?.response?.data?.message))
     dispatch(setAppReady(true))
   }
 }
@@ -44,13 +41,11 @@ export const authUser = () => async (dispatch) => {
   if (data.message) {
     setAuthError(data.message)
     dispatch(setAppReady(true))
-    await localStorage.removeItem('authToken')
     deleteCookie('authToken')
     setIsAuth({ isAuth: false })
     dispatch(toggleIsLoading(false))
   } else {
     dispatch(setUserAuthData(data.user))
-    localStorage.setItem('authToken', data.token)
     dispatch(setAppReady(true))
     dispatch(toggleIsLoading(false))
   }
@@ -60,7 +55,6 @@ export const authGoogle = (userData) => async (dispatch) => {
   try {
     dispatch(setAppReady(false))
     dispatch(setUserAuthData(userData.user))
-    localStorage.setItem('authToken', userData.token)
     dispatch(setAppReady(true))
   } catch (error) {
     console.log(error)
